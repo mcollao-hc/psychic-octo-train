9dfa398debdd3f09786d5207f6aba6efa207ddba
Mark Collao <106274486+mcollao-hc@users.noreply.github.com>
2025-11-21 22:09:42 -0600

    bad

--- bad.go
+++ bad.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"log"
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
-	"net/http"
 )
 
-func serve() {
-	http.HandleFunc("/redir", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		http.Redirect(w, r, r.Form.Get("target"), 302)
-	})
-	http.HandleFunc("/yeet", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		http.Redirect(w, r, r.Form.Get("target"), 302)
-	})
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		user := r.Form.Get("user")
-		pw := r.Form.Get("password")
-
-		log.Printf("Registering new user %s with password %s.\n", user, pw)
-	})
-	http.ListenAndServe(":80", nil)
+func main() {
+	//Generate Private Key
+	pvk, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(pvk)
 }
-
