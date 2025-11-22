b813cb5d7550eaa10e3da73c71f9106b549ed154
Mark Collao <106274486+mcollao-hc@users.noreply.github.com>
2025-11-21 22:04:33 -0600

    Implement user registration logging
    Added user registration logging functionality.

--- bad.go
+++ bad.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"net/http"
 )
@@ -14,5 +15,13 @@ func serve() {
 		r.ParseForm()
 		http.Redirect(w, r, r.Form.Get("target"), 302)
 	})
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		user := r.Form.Get("user")
+		pw := r.Form.Get("password")
+
+		log.Printf("Registering new user %s with password %s.\n", user, pw)
+	})
+	http.ListenAndServe(":80", nil)
 }
 
